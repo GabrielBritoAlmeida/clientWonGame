451b5cebecd59837a19392381e341c9b1891fa09
gabriel.almeida <gabriel.almeida@vexpro.com.br>
2020-09-19 08:41:26 -0300

    #13 - Size in line colors

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,14 +1,29 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
-import { HeadingProps } from '.'
+import { HeadingProps, LineColorsProps } from '.'
 
 const wrapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
-    padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.colors.secondary};
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+
+    &::after {
+      width: 3rem;
+    }
   `,
 
-  lineBottom: (theme: DefaultTheme) => css`
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xlarge};
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `};
+  `,
+
+  lineLeft: (theme: DefaultTheme, lineColor: LineColorsProps) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors[lineColor]};
+  `,
+
+  lineBottom: (theme: DefaultTheme, lineColor: LineColorsProps) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
 
@@ -18,21 +33,19 @@ const wrapperModifiers = {
       bottom: -1rem;
       content: '';
       width: 5rem;
-      border-bottom: 0.6rem solid ${theme.colors.primary};
+      border-bottom: 0.6rem solid ${theme.colors[lineColor]};
     }
   `
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
-    font-size: ${theme.font.sizes.xlarge};
+  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.colors[color!]};
 
-    ${media.greaterThan('medium')`
-      font-size: ${theme.font.sizes.xxlarge};
-    `};
-
-    ${lineLeft && wrapperModifiers.lineLeft(theme)}
-    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor)}
+    ${lineBottom &&
+    wrapperModifiers.lineBottom(theme, lineColor)}
+    ${!!size &&
+    wrapperModifiers[size](theme)}
   `}
 `
--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -19,21 +19,46 @@ describe('<Heading />', () => {
     })
   })
 
-  it('should render a heading with a line to the left side', () => {
-    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'font-size': '1.6rem'
     })
-  })
 
-  it('should render a heading with a line to the bottom', () => {
-    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-      'border-bottom',
-      '0.6rem solid #F231A5',
+      'width',
+      '3rem',
       {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.6rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.6rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
