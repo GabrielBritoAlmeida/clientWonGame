a4624ed7e0d9ed00e9cde1f8873881d6bad16c9b
gabriel.almeida <gabriel.almeida@vexpro.com.br>
2020-10-05 19:20:07 -0300

    Banner Styles

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,17 +3,18 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    const props = {
-      img: 'https://source.unsplash.com/user/willianjusten/1042x580',
-      title: 'Defy death',
-      subtitle: '<p>Play the new <strong>CrashLands</strong> season',
-      buttonLabel: 'Buy now',
-      buttonLink: '/games/defy-death'
-    }
+    const { container } = renderWithTheme(<Banner {...props} />)
 
-    renderWithTheme(<Banner {...props} />)
     // Verifique se o title existe renderizado (.toBeIntheDocument())
     expect(
       screen.getByRole('heading', { name: /defy death/i })
@@ -23,6 +24,9 @@ describe('<Banner />', () => {
       screen.getByRole('heading', { name: /play the new/i })
     ).toBeInTheDocument()
     // verifique se a imagem existe renderizada
+
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
