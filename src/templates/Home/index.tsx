14060b98cca6af4c1a21eccc9b2c21a1ea9ecfc9
gabriel.almeida <gabriel.almeida@vexpro.com.br>
2020-12-28 18:32:50 -0300

    fix/Home - Menu and Footer

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,8 +1,17 @@
+import { Container } from 'components/Container'
+import Menu from 'components/Menu'
+import Footer from 'components/Footer'
+
 import * as S from './styles'
 
 const Home = () => (
   <S.Wrapper>
-    <h1>Home</h1>
+    <Container>
+      <Menu />
+    </Container>
+    <Container>
+      <Footer />
+    </Container>
   </S.Wrapper>
 )
 
