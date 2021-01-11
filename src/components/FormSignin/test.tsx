812ce234d6d1999c3e8863ee7e03efbef71cbc9b
Gabriel Francelino <gabrielfranccelino@gmail.com>
2021-01-11 11:29:50 -0300

    test: formSignin

--- src/components/FormSignin/test.tsx
+++ src/components/FormSignin/test.tsx
@@ -1,11 +1,41 @@
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import FormSignin from '.'
+import FormSignIn from '.'
 
-describe('<FormSignin />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<FormSignin />)
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
 
-    // expect(container.firstChild).toMatchSnapshot()
+    // verifique email
+    const email = screen.getByPlaceholderText(/email/i)
+    expect(email).toBeInTheDocument()
+
+    // verifique password
+    const password = screen.getByPlaceholderText(/password/i)
+    expect(password).toBeInTheDocument()
+
+    // verifique button
+    const button = screen.getByText(/sign in now/i)
+    expect(button).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    const forgot = screen.getByText(/forgot your password?/i)
+    expect(forgot).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    // text
+    const account = screen.getByText(/don’t have an account\?/i)
+    expect(account).toBeInTheDocument()
+
+    // link
+    const link = screen.getByRole('link', { name: /sign up/i })
+    expect(link).toBeInTheDocument()
   })
 })
