21304c51714598f09f82bae648d381f8f20d6619
Gabriel Francelino <gabrielfranccelino@gmail.com>
2021-01-11 12:11:50 -0300

    refactor: form pages Sign-in and Sign-up

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignin from 'components/FormSignin'
 
 export default function SignIn() {
-  return <Auth title="Sign-in">children</Auth>
+  return (
+    <Auth title="Sign-in">
+      <FormSignin />
+    </Auth>
+  )
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignUp from 'components/FormSignUp'
 
 export default function SignIn() {
-  return <Auth title="Sign-up">children</Auth>
+  return (
+    <Auth title="Sign-up">
+      <FormSignUp />
+    </Auth>
+  )
 }
